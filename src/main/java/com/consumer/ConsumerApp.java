bbc017bc4c130ce5ea66dda815605da622e520f7
Art <you@example.com>
2020-11-21 13:38:52 +0200

    Update ConsumerApp.java

--- src/main/java/com/consumer/ConsumerApp.java
+++ src/main/java/com/consumer/ConsumerApp.java
@@ -1,11 +1,21 @@
 package com.consumer;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class ConsumerApp
 {
+
+	@GetMapping("/")
+	public String index(final Banner.Mode m )
+	{
+		return "index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApp.class, args);
