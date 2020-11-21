5a7a20f3d4a9a91b9d3b083824f070b869d4b970
Art <you@example.com>
2020-11-21 13:57:39 +0200

    Add ConsumerAppTests.java

--- src/test/java/com/consumer/ConsumerAppTests.java
+++ src/test/java/com/consumer/ConsumerAppTests.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+public class ConsumerAppTests {
+    @Test
+    void contextLoads() {
+    }
 }
