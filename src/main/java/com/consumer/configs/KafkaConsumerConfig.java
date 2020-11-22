25362db22bac2ced036f9f0b30fec406b3190050
Art <you@example.com>
2020-11-22 13:55:49 +0200

    Update KafkaConsumerConfig.java

--- src/main/java/com/consumer/configs/KafkaConsumerConfig.java
+++ src/main/java/com/consumer/configs/KafkaConsumerConfig.java
@@ -24,7 +24,8 @@ public class KafkaConsumerConfig
     private static final String groupId = "user-tracking-consumer";
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, String> consumerFactory()
+    {
         Map<String, Object> props = new HashMap<>();
         props.put( ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress );
         props.put( ConsumerConfig.GROUP_ID_CONFIG,  groupId );
@@ -35,10 +36,10 @@ public class KafkaConsumerConfig
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String>
-    kafkaListenerContainerFactory() {
+    kafkaListenerContainerFactory()
+    {
 
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
