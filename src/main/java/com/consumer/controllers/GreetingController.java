7c6c985dfe0c8fc1d877f767ce180fbf8e9d8b04
Art <you@example.com>
2020-11-22 14:15:56 +0200

    Update GreetingController.java

--- src/main/java/com/consumer/controllers/GreetingController.java
+++ src/main/java/com/consumer/controllers/GreetingController.java
@@ -7,11 +7,10 @@ import org.springframework.web.util.HtmlUtils;
 @Controller
 public class GreetingController
 {
-
-
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
+    public Greeting greeting(HelloMessage message) throws Exception
+    {
 //        Thread.sleep(1000); // simulated delay
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
