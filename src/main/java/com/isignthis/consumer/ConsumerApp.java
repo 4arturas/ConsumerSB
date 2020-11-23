29761fa65774aa2dcf952cc3981745a3e14ae30b
Art <you@example.com>
2020-11-23 20:26:45 +0200

    Update ConsumerApp.java

--- src/main/java/com/isignthis/consumer/ConsumerApp.java
+++ src/main/java/com/isignthis/consumer/ConsumerApp.java
@@ -1,13 +1,13 @@
 package com.isignthis.consumer;
 
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@EnableAsync
+//@EnableScheduling
+//@EnableAsync
 public class ConsumerApp
 {
 	public static void main(String[] args)
